012e39177f9d284e9e8344911c34dc50fef0dd16
zerok06 <jose.geeksjose@gmail.com>
2024-11-18 23:15:01 -0500

    cambios

--- app.js
+++ app.js
@@ -1,47 +1,33 @@
-import { degrees, PDFDocument, rgb, StandardFonts } from 'pdf-lib'
+import { degrees, PDFDocument, rgb, StandardFonts, } from 'pdf-lib'
 import data from './json/a.json' assert { type: 'json' }
 import fontkit from '@pdf-lib/fontkit'
 
 import { readFileSync, writeFileSync } from 'fs'
 
-data.forEach(async ({ id, nombres, apellido_materno, apellido_paterno }) => {
+data.forEach(async ({ id, firstName, lastName }) => {
   const existingPdfBytes = readFileSync(`./pdf/certificado.pdf`)
   const pdfDoc = await PDFDocument.load(existingPdfBytes)
   pdfDoc.registerFontkit(fontkit)
-  const fontBytes = readFileSync('./font/opensans.ttf')
+  const fontBytes = readFileSync('./font/Lato-Regular.ttf')
   const customFont = await pdfDoc.embedFont(fontBytes)
-  const helveticaFont = await pdfDoc.embedFont(StandardFonts.Helvetica)
   const pages = pdfDoc.getPages()
   const firstPage = pages[0]
   const { width, height } = firstPage.getSize()
-  const nombre = nombres
+  const nombre = firstName
+    .concat(" ", lastName)
     .split(' ')
-    .map(
-      item => item.toLocaleUpperCase()[0] + item.toLocaleLowerCase().slice(1)
-    )
+    .map(item => item.toLocaleUpperCase()[0] + item.toLocaleLowerCase().slice(1))
     .join(' ')
-  const apellido = (apellido_materno + ' ' + apellido_paterno)
-    .split(' ')
-    .map(
-      item => item[0].toLocaleUpperCase() + item.toLocaleLowerCase().slice(1)
-    )
-    .join(' ')
+
   firstPage.drawText(nombre, {
-    x: (width - 240) / 2 + 240 - (nombre.length * 35) / 2,
-    y: height / 2 - 300,
-    size: 70,
+    x: width / 2 - customFont.widthOfTextAtSize(nombre, 32) / 2,
+    y: height / 2 + 10,
+    size: 32,
     font: customFont,
-    color: rgb(0, 0, 0),
+    color: rgb(0.23921568627450981, 0.25882352941176473, 0.2823529411764706),
     rotate: degrees(0),
   })
-  firstPage.drawText(apellido, {
-    x: (width - 240) / 2 + 240 - (apellido.length * 35) / 2,
-    y: height / 2 - 400,
-    size: 70,
-    font: customFont,
-    color: rgb(0, 0, 0),
-    rotate: degrees(0),
-  })
+
   const pdfBytes = await pdfDoc.save()
   writeFileSync(`./out/${id}.pdf`, pdfBytes, err => {
     if (err) console.log(err)
