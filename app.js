e47b8161071ebde6c66fe8d09a908d81820a4591
zerok06 <jose.geeksjose@gmail.com>
2024-12-06 00:56:33 -0500

    cambios

--- app.js
+++ app.js
@@ -4,32 +4,32 @@ import fontkit from '@pdf-lib/fontkit'
 
 import { readFileSync, writeFileSync } from 'fs'
 
-data.forEach(async ({ id, firstName, lastName }) => {
+data.forEach(async ({ id, Nombres, Apellidos, Email }) => {
   const existingPdfBytes = readFileSync(`./pdf/certificado.pdf`)
   const pdfDoc = await PDFDocument.load(existingPdfBytes)
   pdfDoc.registerFontkit(fontkit)
-  const fontBytes = readFileSync('./font/Lato-Regular.ttf')
+  const fontBytes = readFileSync('./font/ArialCEBoldItalic.ttf')
   const customFont = await pdfDoc.embedFont(fontBytes)
   const pages = pdfDoc.getPages()
   const firstPage = pages[0]
   const { width, height } = firstPage.getSize()
-  const nombre = firstName
-    .concat(" ", lastName)
+  const nombre = Nombres
+    .concat(" ", Apellidos)
     .split(' ')
     .map(item => item.toLocaleUpperCase()[0] + item.toLocaleLowerCase().slice(1))
     .join(' ')
 
   firstPage.drawText(nombre, {
-    x: width / 2 - customFont.widthOfTextAtSize(nombre, 32) / 2,
-    y: height / 2 + 10,
-    size: 32,
+    x: width / 2 - customFont.widthOfTextAtSize(nombre, 26) / 2,
+    y: height / 2 + 26,
+    size: 26,
     font: customFont,
-    color: rgb(0.23921568627450981, 0.25882352941176473, 0.2823529411764706),
+    color: rgb(0, 0, 0),
     rotate: degrees(0),
   })
 
   const pdfBytes = await pdfDoc.save()
-  writeFileSync(`./out/${id}.pdf`, pdfBytes, err => {
+  writeFileSync(`./out/${id}-${nombre}-${Email}.pdf`, pdfBytes, err => {
     if (err) console.log(err)
     else {
       console.log('File written successfully\n')
